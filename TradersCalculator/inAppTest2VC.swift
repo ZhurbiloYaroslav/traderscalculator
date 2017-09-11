bf6b05139e4c83956034e42c8cafd59c5a7eafa6
YaroslavZhurbilo <4872439@gmail.com>
2017-09-11 19:07:21 +0300

    Added ViewController and Button in options for inApp purchase test 2

---
+++ TradersCalculator/inAppTest2VC.swift
@@ -0,0 +1,19 @@
+//
+//  inAppTest2VC.swift
+//  TradersCalculator
+//
+//  Created by Yaroslav Zhurbilo on 11.09.17.
+//  Copyright © 2017 Yaroslav Zhurbilo. All rights reserved.
+//
+
+import UIKit
+
+class inAppTest2VC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+}
