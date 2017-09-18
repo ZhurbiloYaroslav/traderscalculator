20b70b3efcb8576762b260830cc45b8f93bef436
YaroslavZhurbilo <4872439@gmail.com>
2017-09-18 20:38:07 +0300

    Started to make a Greetings Screen

---
+++ TradersCalculator/GreetingsVC.swift
@@ -0,0 +1,19 @@
+//
+//  GreetingsVC.swift
+//  TraderCalculator
+//
+//  Created by Yaroslav Zhurbilo on 18.09.17.
+//  Copyright © 2017 Yaroslav Zhurbilo. All rights reserved.
+//
+
+import UIKit
+
+class GreetingsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+}
